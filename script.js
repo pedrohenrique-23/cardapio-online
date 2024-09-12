e91ff46791afb810cbd102c7262288e68053443b
Pedro Henrique <pedrosilva10057@gmail.com>
2024-09-12 16:25:34 -0300

    feat: created function that checks time

--- script.js
+++ script.js
@@ -136,3 +136,10 @@ checkoutBtn.addEventListener("click", function(){
     }
 })
 
+// Verificar a hora e manipular o card horário
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22; 
+}
+
