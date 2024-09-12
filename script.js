9d650c3c7d41f5ed622fdf6312afe8fc01e8e266
Pedro Henrique <pedrosilva10057@gmail.com>
2024-09-12 16:55:55 -0300

    feat: sends to whatsapp api if the restaurant is open and displays a toastify js alert if it is closed

--- script.js
+++ script.js
@@ -127,13 +127,45 @@ addressInput.addEventListener("input", function(event){
     }
 })
 
+// Finalizar pedido
 checkoutBtn.addEventListener("click", function(){
+    const isOpen = checkRestaurantOpen();
+
+    if(!isOpen){
+        Toastify({
+            text: "Ops! O restaurante está fechado.",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "left", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "#ef4444",
+            },
+        }).showToast();
+        return;
+    }
+
     if(cart.length === 0) return;
     if(addressInput.value === ""){
         addressWarn.classList.remove("hidden")
         addressInput.classList.add("border-red-500")
         return;
     }
+
+    //Enviar o pedido para a api wpp
+    const cartItems = cart.map((item) => {
+        return(
+           `${item.name} - Quantidade: (${item.quantity}) - Preço: R$ ${item.price} |` 
+        )
+    }).join("")
+
+    const message = encodeURIComponent(cartItems)
+    const phone = "88993118650"
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
+
+    cart = [];
+    updateCartModal();
 })
 
 // Verificar a hora e manipular o card horário
